cf352e0f64c5a04933e84821fe352260950a71b4
Tanzeela Fatima <149133736+TanzeelaFatima492@users.noreply.github.com>
2025-11-23 13:26:00 +0500

    Refactor background.js for improved clarity

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,13 +1,14 @@
-const bg = document.getElementById('animated-bg');
 
-for(let i = 0; i < 20; i++){
+const bg = document.getElementById("animated-bg");
+
+for (let i = 0; i < 20; i++) {
     let c = document.createElement("div");
     let size = Math.random() * 80 + 20;
-    c.classList.add("circle");
+    c.classList.add("bg-circle");
     c.style.width = size + "px";
     c.style.height = size + "px";
     c.style.left = Math.random() * window.innerWidth + "px";
-    c.style.top = Math.random() * window.innerHeight + "px"; // make them visible immediately
+    c.style.top = Math.random() * window.innerHeight + "px";
     c.style.animationDuration = Math.random() * 5 + 8 + "s";
     bg.appendChild(c);
 }
